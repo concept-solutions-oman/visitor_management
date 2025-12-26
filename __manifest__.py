8aecb4a1f46873c6a3ea69c227100c6d8f7049b7
Ashwin-A21 <djashwinsalian@gmail.com>
2025-12-26 09:35:12 +0530

    Update to OPL-1 license and add professional banner for Odoo Store

--- __manifest__.py
+++ __manifest__.py
@@ -36,7 +36,7 @@ New Visitor Pass Workflow with Hybrid Kiosk:
     'installable': True,
     'application': True,
     'auto_install': False,
-    'license': 'LGPL-3',
+    'license': 'OPL-1',
     'price': 50.00,
     'currency': 'USD',
 }
